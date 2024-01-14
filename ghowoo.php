7cfc38cee7f2650e422ec238f6be5373786f1e75
malawad <50057882+malawadd@users.noreply.github.com>
2024-01-14 21:49:49 +0300

    ❄️ ghowoo init ❄️

---
+++ ghowoo.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name:GhoWoo 
+ * Description:Use MataMask payment gateway for WooCommerce store and let customers pay with ETH and GHO.
+ * Author:LFGHO HACKTHON
+ * Author URI:https://ethglobal.com/
+ * Version: 2.0.0
+ * License: GPL2
+ * Text Domain: gho
+ * Domain Path: /languages
+ *
+ * @package Gho-Woo
+ */
+
